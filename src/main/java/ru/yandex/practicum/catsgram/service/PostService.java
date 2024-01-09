91eaa16c5b5fd2dac726fd295653e9707dd62f70
Brel Snezhana <brelok@1-pp.ru>
2024-01-09 18:49:02 +0300

    Спринт 10/20: 10 → Тема 2/4: Параметры запроса и формат ответа → Урок 2/3

--- src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -7,6 +7,7 @@ import ru.yandex.practicum.catsgram.model.Post;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class PostService {
@@ -24,8 +25,14 @@ public class PostService {
         return globalId++;
     }
 
-    public List<Post> findAll() {
-        return posts;
+    public List<Post> findAll(Integer size, Integer from, String sort) {
+        return posts.stream().sorted((p0, p1) -> {
+            int comp = p0.getCreationDate().compareTo(p1.getCreationDate()); //прямой порядок сортировки
+            if(sort.equals("desc")){
+                comp = -1 * comp; //обратный порядок сортировки
+            }
+            return comp;
+        }).skip(from).limit(size).collect(Collectors.toList());
     }
 
     public Post create(Post post) throws UserNotFoundException {
